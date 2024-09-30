7000bd875980fa53f05774bed39cb145a03a513b
MuhammadSaqib-1 <visitingknowledge@gmail.com>
2024-09-30 15:49:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function calculate() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value)
+    const SecondNumber = parseFloat(document.getElementById("SecondNumber").value)
+    const operator = document.getElementById("operator").value
+
+let result ;
+if(operator === "Addition" ){
+result = firstNumber + SecondNumber
+}
+else if (
+    operator === "Subtraction"
+)
+{result = firstNumber - SecondNumber}
+else if (
+    operator === "Multiplication"
+)
+{result = firstNumber * SecondNumber}
+
+else if (
+    operator === "Division"
+
+)
+   {result = firstNumber / SecondNumber}
+
+   else {"Please select a valid operator"}
+
+document.getElementById("result").innerText ="Result: " + result;
+}
